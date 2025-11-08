b8433779044b22cc93fbdf7fe2bce4d9c1cd5726
Goodluck <godspowergoodluck123@gmail.com>
2025-11-08 09:13:43 +0100

    Add 1-multi_languages.js script

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,7 +1,4 @@
-const line1 = "C is fun";
-const line2 = "Python is cool";
-const line3 = "JavaScript is amazing";
-
-console.log(line1);
-console.log(line2);
-console.log(line3);
+#!/usr/bin/node
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
