9f58049da9b7d7a344e17a4dbe4d54ea302bb845
Goodluck <godspowergoodluck123@gmail.com>
2025-11-08 11:37:16 +0100

    Add 8-square.js script to print square of X

---
+++ 8-square.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const size = parseInt(process.argv[2], 10);
+
+if (!size || size < 1) {
+  console.log('Missing size');
+} else {
+  const line = 'X'.repeat(size);
+  for (let i = 0; i < size; i++) console.log(line);
+}
