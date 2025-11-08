ffe9833f84e29fd1fc1712d94a042bcd02c88fc3
Goodluck <godspowergoodluck123@gmail.com>
2025-11-08 11:30:30 +0100

    Add 7-multi_c.js script to print C is fun x times

---
+++ 7-multi_c.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const x = parseInt(process.argv[2], 10);
+
+if (!x) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) console.log('C is fun');
+}
